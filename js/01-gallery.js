60ff974445c79b8eb2110bcf853b3970d1ec3f94
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-01-03 13:25:28 +0100

    first task is done

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+// Створення розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+function CreateGalleryItemsMarkup(gallery) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item"><a class="gallery__link" href="${original}">
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+    })
+    .join('');
+}
+console.log(CreateGalleryItemsMarkup(galleryItems));
+
+// Рендеремо розмітку на сторінку
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsMarkup = CreateGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+// Реалізація делегування на div.gallery і отримання url великого зображення (використання бібліотеки ligthbox).
+
+galleryContainer.addEventListener('click', onClickItem);
+
+
+function onClickItem(e) {
+   e.preventDefault();
+   const isPictureItem = e.target.classList.contains('gallery__image');
+   if (!isPictureItem) {
+      return;
+   }
+   const originalImage = basicLightbox.create(`
+        <img src="${e.target.dataset.source}" alt="${e.target.alt}" width="800" height="600"/>
+`);
+   originalImage.show();
+   
+   document.addEventListener('keydown', function ({code}) {
+      if (code === 'Escape')
+        originalImage.close(); 
+   });
+};
+
+
